be8c18ef7d86fb0b134af43944e37aefa305c85f
TofuAndSoySauce <hhr245@naver.com>
2023-06-15 15:32:20 +0900

    hyoungran_0615 프리티어 수정

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@
 // js 내용
 // 팝업을 띄우기 위한 "jenesius-vue-modal"을 사용하기 위해
 // 기본적으로 contianer라는 걸 설정해 줌
-import { container } from "jenesius-vue-modal";
+import { container } from 'jenesius-vue-modal';
 
 /* import Login from "@/views/Login.vue";
 import Dashboard from "@/views/Dashboard.vue"; */
@@ -24,7 +24,7 @@ import Dashboard from "@/views/Dashboard.vue"; */
 // 팝업을 띄우기 위해 components에 container 설정 (decrepated)
 export default {
   components: { WidgetContainerModal: container },
-  name: "app",
+  name: 'app',
 };
 </script>
 
