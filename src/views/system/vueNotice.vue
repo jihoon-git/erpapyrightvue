1c7c18a12c2d2f72802c0516a2a070fba622d774
TofuAndSoySauce <hhr245@naver.com>
2023-06-22 16:11:05 +0900

    공지사항 컨버징 완료. 파일 저장 업데이트 확인

---
+++ src/views/system/vueNotice.vue
@@ -0,0 +1,203 @@
+<template>
+  <p class="Location">
+    <a href="../dashboard/dashboard.do" class="btn_set home">메인으로</a>
+    <span class="btn_nav bold">실습</span>
+    <span class="btn_nav bold">공지사항 관리</span>
+    <a href="../system/notice.do" class="btn_set refresh">새로고침</a>
+  </p>
+  <p class="conTitle">
+    <span>공지사항</span>
+    <span class="fr">
+      제목
+      <input type="text" v-model="scrtitle" />
+      <input type="date" v-model="srcsdate" /> ~
+      <input type="date" v-model="srcedate" />
+      <a class="btnType blue" href="" @click.prevent="schPromotion()"
+        ><span>검색</span></a
+      >
+    </span>
+  </p>
+  <div align="right">
+    <template v-if="this.noticeUserType == 'A'">
+      <a class="btnType blue" href="" @click.prevent="initpopup()"
+        ><span>등록</span></a
+      >
+    </template>
+  </div>
+  <div class="divComGrpCodList">
+    <table class="col">
+      <caption>
+        caption
+      </caption>
+      <colgroup>
+        <col width="6%" />
+        <col width="57%" />
+        <col width="17%" />
+        <col width="20%" />
+      </colgroup>
+      <thead>
+        <tr>
+          <th scope="col">번호</th>
+          <th scope="col">제목</th>
+          <th scope="col">작성자</th>
+          <th scope="col">날짜</th>
+        </tr>
+      </thead>
+      <template v-if="countnoticelist == 0">
+        <tbody>
+          <tr>
+            <td colspan="4">데이터가 존재하지 않습니다.</td>
+          </tr>
+        </tbody>
+      </template>
+      <template v-else>
+        <tbody v-for="(list, index) in grouplist" :key="index">
+          <tr>
+            <td>{{ list.notice_no }}</td>
+            <td>
+              <a href="" @click.prevent="fn_detailone(list)">{{
+                list.notice_title
+              }}</a>
+            </td>
+            <td>{{ list.writer }}</td>
+            <td>{{ list.notice_date }}</td>
+          </tr>
+        </tbody>
+      </template>
+    </table>
+  </div>
+  <div>
+    <paginate
+      class="justify-content-center"
+      v-model="currentPage"
+      :page-count="totalPage"
+      :page-range="5"
+      :margin-pages="0"
+      :click-handler="clickCallback"
+      :prev-text="'Prev'"
+      :next-text="'Next'"
+      :container-class="'pagination'"
+      :page-class="'page-item'"
+    >
+    </paginate>
+  </div>
+</template>
+<script>
+import vueNoticeModal from './vueNoticeModal.vue';
+import Paginate from 'vuejs-paginate-next';
+import { openModal } from 'jenesius-vue-modal';
+export default {
+  data: function () {
+    return {
+      grouplist: [],
+
+      //pageinate 설정
+      currentPage: 1,
+      pageSize: 5,
+      totalPage: 1,
+      countnoticelist: 0, //데이터갯수
+      grdNo: 1,
+      //totalCnt: 0, //데이터갯수
+
+      //admin 여부
+      noticeUserType: '',
+
+      //검색클릭
+      searchKey: '',
+      scrtitle: '',
+      srcsdate: '',
+      srcedate: '',
+    };
+  },
+  mounted() {
+    this.searchnotice();
+
+    this.noticeUserType = this.$store.state.loginInfo.userType;
+    console.log('created userType' + this.noticeUserType);
+  },
+  computed: {},
+  components: {
+    paginate: Paginate,
+  },
+  methods: {
+    getToday: function () {
+      var date = new Date();
+      var year = date.getFullYear();
+      var month = ('0' + (1 + date.getMonth())).slice(-2);
+      var day = ('0' + date.getDate()).slice(-2);
+
+      return year + '-' + month + '-' + day;
+    },
+    //전역변수로 page 설정. 데이터를 page로 나눠주는 역할. 이미 설정
+    //paginate callback
+    clickCallback: function (pageNum) {
+      this.currentPage = pageNum;
+      this.searchnotice();
+    },
+    //검색 클릭시
+    schPromotion: function () {
+      this.searchKey = '';
+
+      const checkDate = this.$checkStartEndDate(this.srcsdate, this.srcedate);
+      if (!checkDate) {
+        // 검색 종료일 초기화
+        this.srcedate = '';
+        return false;
+      }
+      this.searchKey = 'Z';
+      this.searchnotice();
+    },
+    //공지사항 리스트
+    searchnotice: function () {
+      let vm = this;
+      let params = new URLSearchParams();
+      if (this.searchKey == 'Z') {
+        params.append('pageSize', this.pageSize);
+        params.append('cpage', this.currentPage);
+        params.append('scrtitle', this.scrtitle);
+        params.append('srcsdate', this.srcsdate);
+        params.append('srcedate', this.srcedate);
+      } else {
+        params.append('pageSize', this.pageSize);
+        params.append('cpage', this.currentPage);
+      }
+
+      this.$vuecombiListAxios('/system/vueNoticeList.do', params).then(
+        (response) => {
+          vm.grouplist = response.data.vueNoticelist;
+          vm.countnoticelist = response.data.countnoticelist;
+          vm.totalPage = vm.$page(vm.countnoticelist, vm.pageSize);
+        }
+      );
+    },
+    //상세페이지 보기
+    fn_detailone: async function (list) {
+      const modal = await openModal(vueNoticeModal, {
+        receiveObject: list,
+        receiveAction: 'U',
+      });
+      modal.onclose = () => {
+        console.log('Close : ');
+        this.searchnotice();
+      };
+    },
+    //공지 등록
+    initpopup: async function () {
+      const modal = await openModal(vueNoticeModal, {
+        receiveAction: 'I',
+        receiveObject: {
+          writer: this.$store.state.loginInfo.loginId,
+          today: this.getToday(),
+        },
+        //등록버튼누르면 바로 나오게
+        // this.writerfile = this.$store.state.loginInfo.loginId;
+        // this.notice_datefile = this.getToday();
+      });
+      modal.onclose = () => {
+        console.log('Close : ');
+        this.searchnotice();
+      };
+    },
+  },
+};
+</script>
---
+++ src/views/system/vueNoticeModal.vue
@@ -0,0 +1,302 @@
+<template>
+  <div class="layerType2" style="width: 600px">
+    <form id="myForm">
+      <dl>
+        <dt>
+          <strong>공지사항 등록/수정</strong>
+        </dt>
+        <dd class="content">
+          <!-- s : 여기에 내용입력 -->
+          <table class="row">
+            <caption>
+              caption
+            </caption>
+            <colgroup>
+              <col width="120px" />
+              <col width="*" />
+              <col width="120px" />
+              <col width="*" />
+            </colgroup>
+
+            <tbody>
+              <tr>
+                <th scope="row">작성자 <span class="font_red">*</span></th>
+                <td>
+                  <input
+                    type="text"
+                    class="inputTxt p100"
+                    name="writerfile"
+                    v-model="writerfile"
+                    readonly
+                  />
+                </td>
+                <th scope="row">작성일자 <span class="font_red">*</span></th>
+                <td>
+                  <input
+                    type="text"
+                    class="inputTxt p100"
+                    name="notice_datefile"
+                    v-model="notice_datefile"
+                    readonly
+                  />
+                </td>
+              </tr>
+              <tr>
+                <th scope="row">제목 <span class="font_red">*</span></th>
+                <td colspan="3">
+                  <input
+                    type="text"
+                    class="inputTxt p100"
+                    name="notice_titlefile"
+                    v-model="notice_titlefile"
+                  />
+                </td>
+              </tr>
+
+              <tr>
+                <th scope="row">내용 <span class="font_red">*</span></th>
+                <td colspan="3">
+                  <textarea
+                    class="inputTxt p100"
+                    name="notice_detfile"
+                    v-model="notice_detfile"
+                  >
+                  </textarea>
+                </td>
+              </tr>
+
+              <tr>
+                <th scope="row">파일 <span class="font_red">*</span></th>
+                <td>
+                  <!-- input type="file" class="inputTxt p100" name="addfile" id="addfile"  onChange="fn_filechange(event)"  / -->
+                  <input
+                    type="file"
+                    class="inputTxt p100"
+                    name="addfile"
+                    @change="onFileChange"
+                  />
+                </td>
+                <td colspan="2">
+                  <div v-html="fileview" @click="fileclick"></div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+
+          <!-- e : 여기에 내용입력 -->
+
+          <div class="btn_areaC mt30">
+            <template v-if="noticeUserType == 'A'">
+              <a href="" class="btnType blue" @click.prevent="btnSavefile"
+                ><span>저장</span></a
+              >
+              <a
+                href=""
+                class="btnType blue"
+                v-if="this.action == 'U'"
+                @click.prevent="btnDeletefile(this.notice_no)"
+                ><span>삭제</span></a
+              >
+              <a
+                href=""
+                @click.prevent="btnClose"
+                class="btnType gray"
+                id="btnClosefile"
+                ><span>닫기</span></a
+              >
+            </template>
+            <template v-else>
+              <a
+                href=""
+                @click.prevent="btnClose"
+                class="btnType gray"
+                id="btnClosefile"
+                ><span>닫기</span></a
+              >
+            </template>
+          </div>
+        </dd>
+      </dl>
+      <a href="" class="closePop" @click.prevent="btnClose"
+        ><span class="hidden">닫기</span></a
+      >
+    </form>
+  </div>
+</template>
+<script>
+import { closeModal } from 'jenesius-vue-modal';
+
+export default {
+  props: { receiveObject: Object, receiveAction: String },
+  data: function () {
+    return {
+      noticeUserType: '',
+
+      //props로 받은값
+      action: '',
+      noticeObject: {},
+
+      notice_no: '',
+      writerfile: '',
+      notice_datefile: '',
+      notice_titlefile: '',
+      notice_detfile: '',
+      fileview: '',
+
+      filename: '',
+      filephy: '',
+    };
+  },
+  created() {
+    this.noticeObject = this.receiveObject;
+    this.action = this.receiveAction;
+
+    //유저타입
+    this.noticeUserType = this.$store.state.loginInfo.userType;
+
+    //console.log('created noticeObject ' + String.valueOf(this.noticeObject));
+    if (this.action == 'U') {
+      this.notice_no = this.noticeObject.notice_no;
+    }
+  },
+  mounted() {
+    if (this.receiveAction == 'I') {
+      //초기화
+      this.notice_no = '';
+      this.notice_titlefile = '';
+      this.notice_detfile = '';
+
+      //등록시 바로 보이게
+      this.writerfile = this.noticeObject.writer;
+      this.notice_datefile = this.noticeObject.today;
+    } else {
+      this.fn_detailone();
+    }
+  },
+  methods: {
+    onFileChange: function (e) {
+      var files = e.target.files;
+      var fileType = files[0].type;
+      if (
+        fileType == 'image/png' ||
+        fileType == 'image/jpg' ||
+        fileType == 'image/jpeg' ||
+        fileType == 'image/bmp'
+      ) {
+        this.fileview =
+          "<img style='width:200px; height:130px' src='" +
+          window.URL.createObjectURL(e.target.files[0]) +
+          "' />";
+      }
+    },
+    //파일을 클리하면 다운로드
+    fileclick: function () {
+      let vm = this;
+
+      let params = new URLSearchParams();
+      // params.append('file_madd', this.filephy);
+      // params.append('file_name', this.filename);
+      params.append('notice_no', this.noticeObject.notice_no);
+      this.$vuecombiListAxios('/system/noticefiledownaload.do', params).then(
+        (res) => {
+          if (res.status == 200) {
+            //  console.log(res);
+            var FILE = window.URL.createObjectURL(new Blob([res.data]));
+            // console.log('FILE : ' + FILE);
+            var docUrl = document.createElement('a');
+            docUrl.href = FILE;
+            docUrl.setAttribute('download', vm.filename);
+            document.body.appendChild(docUrl);
+            docUrl.click();
+          }
+        }
+      );
+    },
+    //상세조회 클릭시
+    fn_detailone: function () {
+      let vm = this;
+      let params = new URLSearchParams();
+      params.append('notice_no', this.noticeObject.notice_no);
+      console.log('fn_detailone params ' + params);
+      this.$vuecombiListAxios('/system/vueNoticeDetailone.do', params).then(
+        function (response) {
+          console.log('fn_detailone response' + JSON.stringify(response));
+
+          vm.notice_no = response.data.detailone.notice_no;
+          vm.writerfile = response.data.detailone.writer;
+          vm.notice_datefile = response.data.detailone.notice_date;
+          vm.notice_titlefile = response.data.detailone.notice_title;
+          vm.notice_detfile = response.data.detailone.notice_det;
+
+          //vm.imgSrc = res.data.boardinfo.file_nadd;
+          let file_name = response.data.detailone.file_name;
+
+          vm.filename = response.data.detailone.file_name;
+          vm.filephy = response.data.detailone.file_madd;
+
+          //file_name 이 있으면
+          if (file_name) {
+            let file_namearr = file_name.split('.');
+            if (
+              file_namearr[1] == 'png' ||
+              file_namearr[1] == 'jpg' ||
+              file_namearr[1] == 'jpeg' ||
+              file_namearr[1] == 'bmp'
+            ) {
+              vm.fileview =
+                "<img src='" +
+                response.data.detailone.file_nadd +
+                "' style ='width: 200px; height: 130px;' />";
+            } else {
+              vm.fileview = response.data.detailone.file_name;
+            }
+          }
+        }
+      );
+    },
+    btnSavefile: function () {
+      //let vm = this;
+      let formData = new FormData(document.getElementById('myForm'));
+      formData.enctype = 'multipart/form-data';
+
+      if (this.receiveAction == 'I') {
+        formData.append('action', 'I');
+        formData.append('loginID', this.$store.state.loginInfo.loginId);
+        formData.append('writerfile', this.writerfile);
+        formData.append('notice_titlefile', this.notice_titlefile);
+        formData.append('notice_detfile', this.notice_detfile);
+
+        this.$vuecombiListAxios('/system/noticesavefile.do', formData).then();
+        alert('저장 되었습니다.');
+        closeModal();
+      } else if (this.receiveAction == 'U') {
+        formData.append('action', 'U');
+        formData.append('noticeno', this.notice_no);
+        formData.append('loginID', this.$store.state.loginInfo.loginId);
+        formData.append('writerfile', this.writerfile);
+        formData.append('notice_titlefile', this.notice_titlefile);
+        formData.append('notice_detfile', this.notice_detfile);
+
+        this.$vuecombiListAxios('/system/noticesavefile.do', formData).then();
+
+        alert('수정 되었습니다.');
+        closeModal();
+      }
+    },
+    btnDeletefile: function (notice_no) {
+      //let vm = this;
+      let params = new URLSearchParams();
+      //console.log('btnDeletefile notice_no' + notice_no);
+      params.append('noticeno', notice_no);
+      params.append('action', 'D');
+      this.$vuecombiListAxios('/system/noticesavefile.do', params).then();
+      alert('삭제 되었습니다');
+      closeModal();
+    },
+
+    btnClose: function () {
+      closeModal();
+    },
+  },
+};
+</script>
